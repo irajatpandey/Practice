d71908027908ec4ca54d622bff3e0fa28aa2e1c5
irajatpandey <44925623+irajatpandey@users.noreply.github.com>
2020-05-03 00:47:36 +0530

    done

---
+++ Tree/Inorder_Recusive.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class BinaryTree{
+    public:
+    int data;
+    BinaryTree  *left, *right;
+
+    BinaryTree(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+    
+};
+
+
+void inorder(BinaryTree *root){
+    
+}
+int main(){
+
+    // binary tree formation 
+    BinaryTree* root = new BinaryTree(4);   /*        4        */
+    root->left = new BinaryTree(2);         /*       / \       */
+    root->right = new BinaryTree(5);        /*      2   5      */
+    root->left->left = new BinaryTree(1);   /*     / \ / \     */
+    root->left->right = new BinaryTree(1);  /*    7  1 2  3    */
+    root->right->left = new BinaryTree(2);  /*      /          */
+    root->right->right = new BinaryTree(3); /*     6           */
+    root->left->right->left = new BinaryTree(6); 
+    root->right->left -> left = new BinaryTree(1);
+    
+    inorder(root);
+
+
+
+}
