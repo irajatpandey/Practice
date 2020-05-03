351120d672778544c38175affa86f111559421a7
irajatpandey <44925623+irajatpandey@users.noreply.github.com>
2020-05-03 15:22:02 +0530

    Update LevelOrder_Iterative.cpp

--- Tree/LevelOrder_Iterative.cpp
+++ Tree/LevelOrder_Iterative.cpp
@@ -15,23 +15,7 @@ class BinaryTree{
 
 
 void levelOrder(BinaryTree *root){
-    if(root == NULL) return;
-     queue<BinaryTree *> q;
-
-     q.push(root);
-     while(!q.empty()){
-        BinaryTree *temp = q.front();
-        cout << temp -> data << " ";
-        q.pop();
-
-        if(temp -> left)
-           q.push(temp -> left);
-        if(temp -> right)
-            q.push(temp -> right);
-
-                    
-     }
-
+   
     
 }
 int main(){
