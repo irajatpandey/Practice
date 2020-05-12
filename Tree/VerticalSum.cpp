9764f84c1a29c879f693cbec7879606166ac484b
irajatpandey <44925623+irajatpandey@users.noreply.github.com>
2020-05-12 16:43:21 +0530

    Add files via upload

---
+++ Tree/VerticalSum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class BinaryTree{
+    public:
+    int data;
+    BinaryTree  *left, *right;
+
+    BinaryTree(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+    
+};
+
+
+void verticalSum(BinaryTree *root){
+    
+}
+int main(){
+
+    BinaryTree* root = new BinaryTree(4);   /*        4        */
+    root->left = new BinaryTree(2);         /*       / \       */
+    root->right = new BinaryTree(5);        /*      2   5      */
+    root->left->left = new BinaryTree(1);   /*     / \ / \     */
+    root->left->right = new BinaryTree(1);  /*    1  1 2  3    */
+    root->right->left = new BinaryTree(2);  /*      / /        */
+    root->right->right = new BinaryTree(3); /*     6  1        */
+    root->left->right->left = new BinaryTree(6); 
+    root->right->left -> left = new BinaryTree(1);
+    
+    verticalSum(root);
+
+
+
+}
