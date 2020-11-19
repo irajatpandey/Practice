e4cc72cac55527f415766b5faa99f77de2cd43b9
Rajat Pandey <2018pgcaca38@nitjsr.ac.in>
2020-11-20 01:37:14 +0530

    commit

--- Backtracking/NQueesn.cpp
+++ Backtracking/NQueesn.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 void printNQueen(int **chess, int n){
-    for(int i = 0; i < n; i++){
+     if()
         for(int j = 0; j < n; j++){
             chess[i][j] = 1;
             printNQueen(chess, r)
 
         }
-    }
+    
 }
 int main(){
     cout << "Enter the length of the chess board\n";
